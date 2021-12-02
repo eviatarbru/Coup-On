14cd470a708465bfbe5c8db89b9015e4c94896d5
eviatarbru <eviatarbru@gmail.com>
2021-12-02 13:05:01 +0200

    Registration Screen

---
+++ app/src/main/java/com/shashank/platform/loginui/RegisterActivity.java
@@ -0,0 +1,78 @@
+package com.shashank.platform.loginui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private TextView textView;
+    private EditText email;
+    private EditText password;
+    private Button signUpButton;
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        this.imageView = findViewById(R.id.imageView);
+        this.textView = findViewById(R.id.textView);
+        this.email = findViewById(R.id.email);
+        this.password = findViewById(R.id.password);
+        this.signUpButton = findViewById(R.id.signUpButton);
+        imageView.setOnTouchListener(new OnSwipeTouchListener(getApplicationContext()) {
+            public void onSwipeTop() {
+            }
+
+            public void onSwipeRight() {
+                if (count == 0) {
+                    imageView.setImageResource(R.drawable.good_night_img);
+                    textView.setText("Night");
+                    count = 1;
+                } else {
+                    imageView.setImageResource(R.drawable.good_morning_img);
+                    textView.setText("Morning");
+                    count = 0;
+                }
+            }
+
+            public void onSwipeLeft() {
+                if (count == 0) {
+                    imageView.setImageResource(R.drawable.good_night_img);
+                    textView.setText("Night");
+                    count = 1;
+                } else {
+                    imageView.setImageResource(R.drawable.good_morning_img);
+                    textView.setText("Morning");
+                    count = 0;
+                }
+            }
+
+            public void onSwipeBottom() {
+            }
+
+        });
+
+    }
+
+    public void signUpListener(View view)
+    {
+        System.out.println(this.email.getText());
+        System.out.println(this.password.getText());
+    }
+
+}
